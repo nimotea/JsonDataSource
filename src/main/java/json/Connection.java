1b04e10744c4bfae56aaab4beb1facd564d31ca6
Hannes.Han <hannes.han@grapecity.com>
2023-09-22 16:00:40 +0800

    update

--- src/main/java/json/Connection.java
+++ src/main/java/json/Connection.java
@@ -10,6 +10,6 @@ import lombok.Data;
 public class Connection {
    private String SIP="0";
    private String SourceType="Web";
-   private WebConfigWithMultiBaseURI webConfigWithMultiBaseURI;
+   private WebConfigWithMultiBaseURI webConfigWithMultiBaseURI = new WebConfigWithMultiBaseURI();
    private SchemaDefinition schemaDefinition;
 }
